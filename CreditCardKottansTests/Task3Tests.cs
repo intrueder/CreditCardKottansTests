504874b2f61b2166f56b453bab67a4cbafefc6ae
intrueder <intrueder@gmail.com>
2016-05-07 13:20:29 +0300

    Update Task3Tests - pass test on any exception other than AssertionException - add cases when original number contains digit 9

--- CreditCardKottansTests/Task3Tests.cs
+++ CreditCardKottansTests/Task3Tests.cs
@@ -11,6 +11,8 @@ namespace CreditCardKottansTests
     public class Task3Tests
     {
         [TestCase("343434343434343", "343434343434350")]
+        [TestCase("343434343434392", "343434343434400")]
+        [TestCase("343434343434590", "343434343434608")]
         public void GenerateNextCreditCardNumber_ResultsWithNextNumber(string original, string next)
         {
             Assert.AreEqual(next, CreditCardUtility.GenerateNextCreditCardNumber(original));
@@ -19,7 +21,17 @@ namespace CreditCardKottansTests
         [TestCase("4999999999999999993", "5000000000000000005")]
         public void GenerateNextCreditCardNumber_WithBoundNumber_ResultsWithError(string original, string next)
         {
-            Assert.AreNotEqual(next, CreditCardUtility.GenerateNextCreditCardNumber(original));
+            try
+            {
+                string generatedNext = CreditCardUtility.GenerateNextCreditCardNumber(original);
+                Assert.AreNotEqual(next, generatedNext);
+                // to be shure, that code does not generate number less than original
+                Assert.Greater(StringComparer.OrdinalIgnoreCase.Compare(generatedNext, original), 0);
+            }
+            catch (Exception e) when (!(e is AssertionException))
+            {
+                Assert.Pass();
+            }
         }
     }
 }
